c66d1899ffd8a00ceb505a9aaf078b10ff1a3a00
martn-nguyen <martinmnguyen@utexas.edu>
2023-03-28 23:29:44 -0500

    started fleshing out account page

--- my-app/src/components/Account.js
+++ my-app/src/components/Account.js
@@ -1,17 +1,43 @@
+import "../styles/Account.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 export default function Account() {
   return (
-    <div>
-      This is the account page!
-      <br />
-      <Link to="/">
-        <button variant="contained">Login</button>
-      </Link>
-      <br />
+    <div className="Account">
+      <h1>This is the account page!</h1>
+
+      <Grid container align="center" justifyContent="space-around" spacing={3}>
+        <Grid item xs={4}>
+          Search
+        </Grid>
+        <Grid item xs={4}>
+          My Plays
+        </Grid>
+        <Grid item xs={4}>
+          <Link to="/">
+            <Button variant="contained">Logout</Button>
+          </Link>
+        </Grid>
+
+        <Grid item xs={4}>
+          PlayMaker
+        </Grid>
+        <Grid item xs={4}>
+          Play Name
+        </Grid>
+        <Grid item xs={2}>
+          Export
+        </Grid>
+        <Grid item xs={2}>
+          Delete
+        </Grid>
+      </Grid>
+
       <Link to="/play">
-        <button variant="contained">Play</button>
+        <Button variant="contained">Play</Button>
       </Link>
     </div>
   );
