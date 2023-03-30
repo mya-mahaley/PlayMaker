8f692663bfc263bc8b716e6a10e56155cc79f766
myamahaley <mahaleym2@go.lisd.net>
2023-03-29 21:33:32 -0500

    Basic Play Page

---
+++ my-app/src/components/Notes.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import { useState } from "react";
+
+
+export default function Notes(props) {
+  const [players, setPlayers] = useState("");
+  const [stats, setStats] = useState("");
+  const [problems, setProblems] = useState("");
+  const [comments, setComments] = useState("");
+
+  return(
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Notes
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Players</Form.Label>
+            <Form.Control as="textarea" rows={1} onChange={(e) => {setPlayers(e.target.value)}} defaultValue={players}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Statistics</Form.Label>
+            <Form.Control as="textarea" rows={1} onChange={(e) => {setStats(e.target.value)}} defaultValue={stats}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Problems</Form.Label>
+            <Form.Control as="textarea" rows={1} onChange={(e) => {setProblems(e.target.value)}} defaultValue={problems}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Comments</Form.Label>
+            <Form.Control as="textarea" rows={3} onChange={(e) => {setComments(e.target.value)}} defaultValue={comments}/>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  )
+}
--- my-app/src/components/Play.js
+++ my-app/src/components/Play.js
@@ -1,14 +1,59 @@
+import "../styles/Play.css";
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import Notes from "./Notes.js"
+import Form from 'react-bootstrap/Form'
 import { Link } from "react-router-dom";
 
+
 export default function Play() {
+  const [showNotes, setShowNotes] = useState(false);
+  const [title, setTitle] = useState("Play");
   return (
-    <div>
-      This is the play page!
-      <br />
-      <Link to="/account">
-        <button variant="contained">Account</button>
-      </Link>
+    <div>    
+      <Container >
+        <Row className="headerBorder">
+          <Col className="containerBorder" xs lg="3">Buttons</Col>
+          <Col className="containerBorder" >
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Control size="lg" type="text"rows={1} onChange={(e) => {setTitle(e.target.value)}} defaultValue={title}/>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col className="containerBorder"  xs lg="2">Status</Col>
+        </Row>
+        <Row>
+          <Col xs lg="3">
+            <Row className="containerBorder">
+              <h3>Shapes</h3>
+            </Row>
+            <Row className="containerBorder">
+              <h3>Colors</h3>
+            </Row>
+            <Row className="containerBorder">
+              <h3>Templates</h3>
+            </Row> 
+            <Row className="containerBorder">
+              <Button variant="primary" className="NotesButton" onClick={() => setShowNotes(true)}>
+                Show Notes
+              </Button>
+              <Notes
+                show={showNotes}
+                onHide={() => setShowNotes(false)}
+              />
+            </Row>           
+          </Col>
+          <Col className="containerBorder">
+            <h3>Canvas Column</h3>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
