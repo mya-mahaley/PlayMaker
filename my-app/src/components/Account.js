a3569e771dca14f60c0dc8b6a7420903eb4e9d1a
martn-nguyen <martinmnguyen@utexas.edu>
2023-03-29 19:12:17 -0500

    restyling grid for account

--- my-app/src/components/Account.js
+++ my-app/src/components/Account.js
@@ -1,47 +1,55 @@
 import "../styles/Account.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Account() {
   return (
     <div className="Account">
-      <h1>This is the account page!</h1>
+      <Container className="AccountScreen">
+        <Row className="align-items-center">
+          <Col className="AccountComponent">Search</Col>
+          <Col className="AccountComponent">My Plays</Col>
+          <Col className="AccountComponent">
+            <Link to="/">
+              <Button variant="contained">Logout</Button>
+            </Link>
+          </Col>
+        </Row>
 
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} direction="row" justifyContent="center" alignItems="center">
-        <Grid item className="AccountComponent" xs={3}>
-          Search
-        </Grid>
-        <Grid item className="AccountComponent" xs={3}>
-          My Plays
-        </Grid>
-        <Grid item className="AccountComponent" xs={3}>
-          <Link to="/">
-            <Button variant="contained">Logout</Button>
-          </Link>
-        </Grid>
-        <Box width="100%"></Box>
+        <Row className="align-items-center">
+          <Col>
+            <Row>
+              <Col className="AccountComponent">
+                <Button variant="contained">New Folder</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="AccountComponent">All Sketches</Col>
+            </Row>
+          </Col>
+          <Col className="AccountComponent">
+            <Row>
+              <center>Plays</center>
+            </Row>
+            <Row>
+              <Link to="/play">
+                <Button variant="contained">Create New Play</Button>
+              </Link>
+            </Row>
+          </Col>
+        </Row>
 
-        <Grid item className="AccountComponent" xs={3}>
-          PlayMaker
-        </Grid>
-        <Grid item className="AccountComponent" xs={3}>
-          Play Name
-        </Grid>
-        <Grid item className="AccountComponent" xs={1.5}>
-          Export
-        </Grid>
-        <Grid item className="AccountComponent" xs={1.5}>
-          Delete
-        </Grid>
-        <Box width="100%"></Box>
-      </Grid>
-
-      <Link to="/play">
-        <Button variant="contained">Play</Button>
-      </Link>
+        <Row>
+          <Col className="AccountComponent">PlayMaker</Col>
+          <Col className="AccountComponent">Play Name</Col>
+          <Col className="AccountComponent">Export Delete</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
