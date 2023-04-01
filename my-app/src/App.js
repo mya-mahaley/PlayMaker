f7e3820d813fcc637ddf5bc9cb6c51135483c965
martn-nguyen <martinmnguyen@utexas.edu>
2023-03-31 23:25:45 -0500

    change to hashrouter for gh-pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Account from "./components/Account";
 import Play from "./components/Play";
-import { DndProvider, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/account" element={<Account />} />
-        <Route path="/play" element={<DndProvider backend={HTML5Backend}><Play /></DndProvider>} />
+        <Route path="/play" element={<Play />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
