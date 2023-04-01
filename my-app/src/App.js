f4f38edac08a897000f61eab9675bc93ba4d0f7c
martn-nguyen <martinmnguyen@utexas.edu>
2023-04-01 13:38:40 -0500

    deployment working correctly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Account from "./components/Account";
 import Play from "./components/Play";
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/play" element={<Play />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/play" element={<Play />} />
+    </Routes>
   );
 }
