93d07a4aff6d93544b074e43ceafd2ccf72167f7
Michael Tran <jmtran678@gmail.com>
2023-04-06 16:24:46 -0500

    Fixed background image for template to fill canvas

--- my-app/src/components/Play.js
+++ my-app/src/components/Play.js
@@ -596,7 +596,12 @@ export default function Play() {
           <Col className="containerBorder">
             <canvas
               // background image
-              style={{ backgroundImage: `url(${currentBackground})` }}
+              style={{
+                backgroundImage: `url(${currentBackground})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
               className="Canvas"
               ref={canvasRef}
               onMouseDown={startDrawing}
