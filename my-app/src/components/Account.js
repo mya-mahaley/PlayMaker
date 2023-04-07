0b703817b7629bf05158a913dadc95d460fb0138
mya-mahaley <mya.mahaley@yahoo.com>
2023-04-06 22:01:53 -0500

    Started creating DB

--- my-app/src/components/Account.js
+++ my-app/src/components/Account.js
@@ -7,6 +7,7 @@ import TextField from "@mui/material/TextField";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { auth } from "./login/firebase";
 import { signOut } from "firebase/auth";
@@ -26,67 +27,99 @@ export default function Account() {
 
   return (
     <div className="Account">
-      <Container className="AccountScreen">
-        <Row className="align-items-center">
-          <Col>
-            <TextField
-              id="outlined-basic"
-              label="Search"
-              fullWidth
-              variant="outlined"
-            />
-          </Col>
-          <Col className="AccountComponent">
-            <h1>My Plays</h1>
-          </Col>
-          <Col className="AccountComponent">
-            <Link to="/">
-              <Button variant="contained" onClick={logOut}>Logout</Button>
+    <Container className="AccountScreen">
+      <Row className="align-items-center">
+        <Col xs lg="3">
+          <TextField
+            id="outlined-basic"
+            label="Search"
+            fullWidth
+            variant="outlined"
+          />
+        </Col>
+        <Col className="AccountComponent">
+          <h1>My Plays</h1>
+        </Col>
+        <Col className="AccountComponent">
+          <Link to="/">
+            <Button variant="contained" onClick={logOut}>Logout</Button>
+          </Link>
+          <Button variant="contained">Recently Deleted</Button>
+        </Col>
+      </Row>
+
+      <Row className="align-items-center">
+        <Col xs lg="2">
+          <Row>
+            <Col className="AccountComponent">
+              <Button variant="contained">New Folder</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="AccountComponent">
+              <Button variant="contained">All Sketches</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="AccountComponent">
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+              <Row><Button variant="contained">Folder</Button></Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col className="AccountComponent" >
+          <Row>
+            <center>
+              <h4>
+                <b>Plays</b>
+              </h4>
+            </center>
+          </Row>
+          <Row>
+            <MyPlays></MyPlays>
+          </Row>
+          <Row>
+            <Link to="/play">
+              <Button variant="contained">Create New Play</Button>
             </Link>
-          </Col>
-        </Row>
+          </Row>
+        </Col>
+      </Row>
 
-        <Row className="align-items-center">
-          <Col>
-            <Row>
-              <Col className="AccountComponent">
-                <Button variant="contained">New Folder</Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col className="AccountComponent">
-                <Button variant="contained">All Sketches</Button>
-              </Col>
-            </Row>
-          </Col>
-          <Col className="AccountComponent">
-            <Row>
-              <center>
-                <h4>
-                  <b>Plays</b>
-                </h4>
-              </center>
-            </Row>
-            <Row>
-              <MyPlays></MyPlays>
-            </Row>
-            <Row>
-              <Link to="/play">
-                <Button variant="contained">Create New Play</Button>
-              </Link>
-            </Row>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col className="AccountComponent">
-            <h1>PlayMaker</h1>
-            <img src={logo} width={50} alt="PlayMaker logo"/>
-          </Col>
-          <Col className="AccountComponent">Selected Play Name</Col>
-          <Col className="AccountComponent">Export Delete</Col>
-        </Row>
-      </Container>
-    </div>
+      <Row>
+        <Col className="AccountComponent" xs lg="3">
+          <Row>
+          <Col><h3 className="TextAlignment">PlayMaker</h3></Col>
+          <Col><img src={logo} width={40} alt="PlayMaker logo"/></Col>
+          </Row>
+        </Col>
+        <Col className="align-items-center">
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Control
+                  size="lg"
+                  type="text"
+                  rows={1}
+                  onChange={(e) => {;
+                  }}
+                  defaultValue={"Selected Play"}
+                />
+              </Form.Group>
+            </Form> </Col>
+        <Col className="AccountComponent">
+          <Button variant="contained">Export</Button>
+          <Button variant="contained">Delete</Button>
+        </Col>
+      </Row>
+    </Container>
+  </div>
   );
 }
